75cc86a83543a0d0234071a339df9bc19594745e
wouterheyhey <wouterheyndrickx1@gmail.com>
2016-05-20 12:00:41 +0200

    FAQ en extra properties voor instellingen

---
+++ BL.Domain/FAQ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Domain
+{
+    public class FAQ
+    {
+        public string vraag { get; set; }
+        public string antwoord { get; set; }
+    }
+}
